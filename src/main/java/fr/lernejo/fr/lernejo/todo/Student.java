8c386aa617f84dba331d422bd1eca382d42abcf3
theophane gros <theophane.gros@epita.fr>
2022-10-26 15:55:40 +0200

    partie 3 server is up and running

---
+++ src/main/java/fr/lernejo/fr/lernejo/todo/Student.java
@@ -0,0 +1,10 @@
+package fr.lernejo.fr.lernejo.todo;
+
+import java.time.YearMonth;
+
+public class Student {
+    private String FirstName;
+    private String lastname;
+    private YearMonth birthYearMonth;
+
+}
---
+++ src/main/java/fr/lernejo/fr/lernejo/todo/fr/lernejo/todo/ApplicationIdentifierFilter.java
@@ -0,0 +1,31 @@
+package fr.lernejo.fr.lernejo.todo.fr.lernejo.todo;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ApplicationIdentifierFilter implements javax.servlet.Filter{
+    final String out;
+
+    public ApplicationIdentifierFilter(String out) {
+        this.out = UUID.randomUUID().toString();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+        throws IOException, ServletException {
+        if (response instanceof HttpServletResponse) {
+            ((HttpServletResponse) response).addHeader("instance-id",out);
+        }
+        filterChain.doFilter(request,response);
+
+    }
+}
